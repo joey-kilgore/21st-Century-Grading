74fd3a1aa5ce5bbfe9555c4441873d17d4c64f54
BrendanWMoore <33725495+BrendanWMoore@users.noreply.github.com>
2017-12-16 15:35:51 -0500

    created full ui
    includes ability to

--- WindowsFormsApp1/WpfApp2/MainWindow.xaml.cs
+++ WindowsFormsApp1/WpfApp2/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xaml;
 using Tesseract;
+using WinForms = System.Windows.Forms;
 
 namespace WpfApp2
 {
@@ -21,18 +23,34 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
-            InitializeComponent();
-        }
 
+            Variables.numberOfInputTests = 0;
+            InitializeComponent();
+            
+        }
+        private void resultFileLocationSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            finalFileDestinationTextBox.Text = dialog.SelectedPath;
+        }
+        private void resultFileLocationSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string finalFileDestinationFolder = finalFileDestinationTextBox.Text;
+            finalDestinationSubmitted.IsChecked = true;
+            
+            
+        }
         private void KeySearchButton_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            Nullable<bool> result = dlg.ShowDialog();
+            Microsoft.Win32.OpenFileDialog searchKey = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = searchKey.ShowDialog();
             if (result == true)
             {
-                string filename = dlg.FileName;
+                string filename = searchKey.FileName;
                 KeyTextBox.Text = filename;
             }
         }
@@ -40,6 +58,27 @@ namespace WpfApp2
         private void KeySubmitButton_Click(object sender, RoutedEventArgs e)
         {
             printToConsole(KeyTextBox.Text);
+            KeySubmitted.IsChecked = true;
+            
+            
+        }
+
+        private void StudentSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog searchStudentTests = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = searchStudentTests.ShowDialog();
+            if (result == true)
+            {
+                string filename = searchStudentTests.FileName;
+                StudentsTextBox.Text = filename;
+            }
+        }
+        
+        public void StudentSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            printToConsole(StudentsTextBox.Text);
+            Variables.numberOfInputTests++;
+            studentTestsSubmittedCounter.Content = Variables.numberOfInputTests + " tests have been submitted";
         }
 
         public static void printToConsole(string filePath)
@@ -60,5 +99,14 @@ namespace WpfApp2
                 Console.Read();
             
         }
+
+        private void beginGradingButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            ProblemGrading ProblemGrading = new ProblemGrading();
+            ProblemGrading.Show();
+        }
+        
+        
     }
 }
